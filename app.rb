832a9e14a663ae70cfd9ee9024ede1754931dd6d
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-23 15:00:21 -0800

    Add readme, license and css styles

--- app.rb
+++ app.rb
@@ -19,3 +19,17 @@ post("/stylists") do
   @stylists = Stylists.all()
   erb(:index)
 end
+
+get("/stylists/:id") do
+  @stylists = Stylists.find(params.fetch("id").to_i())
+  erb(:stylists)
+end
+
+post("/clients") do
+  client_name = params.fetch("client_name")
+  stylist_id = params.fetch("stylist_id").to_i()
+  client = Clients.new({:client_name => client_name, :stylist_id => stylist_id})
+  client.save()
+  @stylists = Stylists.find(stylist_id)
+  erb(:stylists)
+end
